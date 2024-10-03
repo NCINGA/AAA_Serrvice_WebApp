51ca33f84a76220faabf0ee8a7f20921019ef53d
shehan <shehan.salinda92@gmail.com>
2024-10-03 14:36:49 +0530

    add paramaters

--- src/Pages/SubscriberCreate.tsx
+++ src/Pages/SubscriberCreate.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useState} from "react";
+import React, {FC, useEffect, useState} from "react";
 import {Button} from "primereact/button";
 import {InputText} from "primereact/inputtext";
 import {Dropdown} from "primereact/dropdown";
@@ -6,33 +6,23 @@ import {Divider} from "primereact/divider";
 import {TabPanel, TabView} from "primereact/tabview";
 import {DataTable} from "primereact/datatable";
 import {Column} from "primereact/column";
-import {GET_PLANS} from "../graphql/queries";
-import {useQuery} from "@apollo/client";
+import {CREATE_NEW_SUBSCRIBER, GET_PLAN_ATTRIBUTES, GET_PLAN_PARAMETERS, GET_PLANS} from "../graphql/queries";
+import {useMutation, useQuery} from "@apollo/client";
 import {IPlan} from "../interface/plan";
 import {InputIcon} from "primereact/inputicon";
 import {IconField} from "primereact/iconfield";
 import {Card} from "primereact/card";
+import {ProgressSpinner} from "primereact/progressspinner";
 
 const SubscriberCreate: FC = () => {
     const [showPassword, setShowPassword] = useState(false);
     const [plan, setPlan] = useState<any>(null);
-    const [products, setProducts] = useState([
-        {code: "001", name: "Test", category: "Test", quantity: 1455}
-    ]);
-    const {loading, error, data} = useQuery(GET_PLANS, {
-        notifyOnNetworkStatusChange: true,
-    });
-    const planOptions = data?.getPlans?.map((plan: IPlan) => ({
-        label: plan?.planName,
-        value: plan?.planId,
-        decs: plan?.description
-    }));
-
-
+    const [localAttributes, setLocalAttributes] = useState([]);
+    const [localParameters, setLocalParameters] = useState([]);
     const [formData, setFormData] = useState({
         username: '',
         password: '',
-        status: null,
+        status: 'INACTIVE',
         contactNo: '',
         email: '',
         extId: '',
@@ -40,14 +30,78 @@ const SubscriberCreate: FC = () => {
         type: '',
         planId: ''
     });
+    const [products, setProducts] = useState([
+        {code: "001", name: "Test", category: "Test", quantity: 1455}
+    ]);
+    const {loading: loadingPlans, error: fetchPlanError, data: plans} = useQuery(GET_PLANS, {
+        notifyOnNetworkStatusChange: true,
+    });
+
+    const {
+        loading: loadingAttribute,
+        error: fetchAttributeError,
+        data: attributes,
+        refetch: refechAttribute
+    } = useQuery(GET_PLAN_ATTRIBUTES, {
+        notifyOnNetworkStatusChange: true,
+        variables: {planId: formData.planId}
+    });
+
+    const {
+        loading: loadingParameters,
+        error: fetchParametersError,
+        data: parameters,
+        refetch: refechParameters
+    } = useQuery(GET_PLAN_PARAMETERS, {
+        notifyOnNetworkStatusChange: true,
+        variables: {planId: formData.planId}
+    });
+
+    useEffect(() => {
+        if (attributes?.getPlanAttribute) {
+            setLocalAttributes(attributes.getPlanAttribute);
+        }
+    }, [attributes]);
+
+
+    useEffect(() => {
+        if (parameters?.getPlanParameter) {
+            setLocalParameters(parameters.getPlanParameter);
+        }
+    }, [parameters]);
+
+
+    const onUpdatePlanOverrideSubscriber = (value, editAttribute) => {
+        const updatedAttributes = localAttributes?.map((attribute) =>
+            attribute.id === editAttribute.id
+                ? {...attribute, attributeOverrideValue: value}
+                : attribute
+        );
+        setLocalAttributes(updatedAttributes);
+    };
+
+    const planOptions = plans?.getPlans?.map((plan: IPlan) => ({
+        label: plan?.planName,
+        value: plan?.planId,
+        decs: plan?.description
+    }));
+
+    const [createSubscriber, {
+        loading: createSubscriberLoder,
+        error: createSubscriberError,
+        data: createSubscriberSuccess
+    }] = useMutation(CREATE_NEW_SUBSCRIBER, {
+        notifyOnNetworkStatusChange: true,
+    });
+
 
     const statusOptions = [
-        { label: 'Active', value: 'active' },
-        { label: 'Inactive', value: 'inactive' },
+        {label: 'Active', value: 'ACTIVE'},
+        {label: 'Inactive', value: 'INACTIVE'},
     ];
     const typeOptions = [
-        { label: 'PPPoE', value: 'PPPoE' },
-        { label: 'Other', value: 'Other' },
+        {label: 'PPPoE', value: 'PPPoE'},
+        {label: 'Other', value: 'Other'},
     ];
 
     const handleInputChange = (e, field) => {
@@ -57,15 +111,59 @@ const SubscriberCreate: FC = () => {
 
     const handleSubmit = () => {
         console.log("Form Data: ", formData);
+        createSubscriber({
+            variables: formData,
+        })
+            .then(response => {
+                console.log('Subscriber created successfully:', response.data);
+            })
+            .catch(err => {
+                console.error('Error creating subscriber:', err);
+            });
     };
 
     const togglePasswordVisibility = () => {
         setShowPassword((prevState) => !prevState);
     };
 
+    const attributeOverrideValue = (data) => (
+        <InputText
+            onChange={(e) => onUpdatePlanOverrideSubscriber(e.target.value, data)}
+            value={data?.attributeOverrideValue}
+            variant="outlined"
+            size="small"
+        />
+    );
+
+    const parameterOverrideValue = (data) => (
+        <InputText
+            onChange={(e) => onUpdatePlanOverrideSubscriber(e.target.value, data)}
+            value={data?.attributeOverrideValue}
+            variant="outlined"
+            size="small"
+        />
+    );
+
+
     return (
         <React.Fragment>
             <div>
+
+                {createSubscriberLoder && <div style={{
+                    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+                    backdropFilter: 'blur(5px)',
+                    position: 'absolute',
+                    top: '0px',
+                    bottom: '0px',
+                    left: '0px',
+                    right: '0px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    zIndex: 9999
+                }}>
+                    <ProgressSpinner style={{width: '50px', height: '50px'}} strokeWidth="2" animationDuration=".5s"/>
+                </div>}
                 <h1>Subscriber Create</h1>
 
                 <div>
@@ -173,14 +271,15 @@ const SubscriberCreate: FC = () => {
                                     <div className="p-field">
                                         <label htmlFor="type">Plan</label>
                                         <Dropdown
-                                            loading={loading}
+                                            loading={loadingPlans}
                                             id="type"
                                             value={formData.planId}
                                             options={planOptions}
                                             onChange={(e) => {
                                                 handleInputChange(e, 'planId');
-                                                const plan = data?.getPlans?.filter((plan: IPlan) => plan.planId == e.target.value)?.[0];
+                                                const plan = plans?.getPlans?.filter((plan: IPlan) => plan.planId == e.target.value)?.[0];
                                                 setPlan(plan);
+                                                refechAttribute
 
 
                                             }}
@@ -207,19 +306,22 @@ const SubscriberCreate: FC = () => {
                                 >
                                     <div style={{ flex: 1 }}>
                                         <h4>Parameter Overrides</h4>
-                                        <DataTable value={products} >
+                                        <DataTable value={localParameters ?? []}>
                                             <Column field="parameterName" header="Parameter Name"></Column>
-                                            <Column field="value" header="Value"></Column>
+                                            <Column field="parameterValue" body={parameterOverrideValue}
+                                                    header="Parameter Value"></Column>
                                             <Column field="OverrideValue" header="Override Value"></Column>
+                                            <Column field="rejectOnFailure" header="Reject On Failure"></Column>
                                         </DataTable>
                                     </div>
 
-                                    <div style={{ flex: 1 }}>
+                                    <div style={{flex: 1}}>
                                         <h4>Attribute Overrides</h4>
-                                        <DataTable value={products}>
-                                            <Column field="parameterName" header="Parameter Name"></Column>
-                                            <Column field="value" header="Value"></Column>
-                                            <Column field="OverrideValue" header="Override Value"></Column>
+                                        <DataTable value={localAttributes ?? []}>
+                                            <Column field="attributeName" header="Attribute Name"></Column>
+                                            <Column field="attributeValue" header=" Attribute Value"></Column>
+                                            <Column field="attributeOverrideValue" header="Override Value"
+                                                    body={attributeOverrideValue}></Column>
                                         </DataTable>
                                     </div>
                                 </div>
--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,5 +1,6 @@
 import {gql} from '@apollo/client';
 
+
 export const GET_SUBSCRIBERS = gql`
     query GetSubscribers($page: Int!, $size: Int!) {
         getSubscribersByPage(page: $page, size: $size) {
@@ -23,10 +24,57 @@ export const GET_SUBSCRIBERS = gql`
     }
 `;
 
+export const CREATE_NEW_SUBSCRIBER = gql`
+    mutation CreateSubscriber($username: String, $password: String, $email: String, $status: String, $contactNo: String, $extId: String, $realm: String, $type: String) {
+        createSubscriber(
+            subscriber: {
+                username: $username
+                password: $password
+                status: $status
+                contactNo: $contactNo
+                email: $email
+                extId: $extId
+                realm: $realm
+                type: $type
+            }
+        )
+    }
+`;
+
+
 export const GET_PLANS = gql`
     query GetPlans {
         getPlans
     }
 
 `;
+
+export const GET_PLAN_ATTRIBUTES = gql`
+    query GetPlanAttribute($planId: Int!) {
+        getPlanAttribute(planId: $planId) {
+            id
+            planId
+            attributeName
+            attributeValue
+            attributeOverrideValue
+        }
+    }
+
+`;
+
+export const GET_PLAN_PARAMETERS = gql`
+    query GetPlanAttribute($planId: Int!) {
+        getPlanParameter(planId: $planId) {
+            parameterId
+            planId
+            parameterName
+            parameterValue
+            rejectOnFailure
+            parameterOverrideValue
+        }
+    }
+
+`;
+
+
 
